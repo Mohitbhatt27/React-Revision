7215faf55d73de8bc477b46832e29f0500d9ecab
Mohit <mohit.peak@gmail.com>
2024-09-05 00:22:27 +0530

    React.createElement

--- 1.BasicsHTML-JS/App.js
+++ 1.BasicsHTML-JS/App.js
@@ -1,7 +1,20 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hi from react"
+const Parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "children" }, [
+    React.createElement("h1", { id: "heading" }, "Nested"),
+    React.createElement("h2", { id: "heading" }, "Sibling"),
+  ])
 );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(Parent);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abc" },
+//   "Hi from react"
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
